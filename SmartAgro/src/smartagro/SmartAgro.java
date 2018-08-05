1521fa0d3bace6e1d2628b469f4eda6878363fda
mbagatini <morganabagatini@gmail.com>
2018-08-05 01:30:14 -0300

    #5
    Foi criado o layput da aplicação e algumas telas básicas

--- SmartAgro/src/smartagro/SmartAgro.java
+++ SmartAgro/src/smartagro/SmartAgro.java
@@ -1,13 +1,26 @@
-
 package smartagro;
+
+import telas.FrmPrincipal;
 
 public class SmartAgro {
 
     public static void main(String[] args) {
+
+        /* Set the Synthetica Plain look and feel */
+        try {
+            javax.swing.UIManager.setLookAndFeel("de.javasoft.plaf.synthetica.SyntheticaPlainLookAndFeel");
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(FrmPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(FrmPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(FrmPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(FrmPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
         
-       System.out.println("Olá mundo.");
-        
-        
+        new FrmPrincipal().setVisible(true);
+
     }
-    
+
 }
