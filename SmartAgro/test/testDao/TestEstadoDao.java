036e5c319231c35aa8301a082a93525de438bda1
Renan Luis Kist <Renan Luis Kist@179.190.67.205>
2018-08-14 12:40:08 -0300

    Ajustando teste.java

--- SmartAgro/test/testDao/TestEstadoDao.java
+++ SmartAgro/test/testDao/TestEstadoDao.java
@@ -93,7 +93,7 @@ public class TestEstadoDao {
         
         GenericDAO<Estado> dao = new GenericDAO();
 
-        ArrayList<Estado> es = dao.consultarComCriterio("Estado", "nome", "Rio Grande do Sul", false);
+        ArrayList<Estado> es = dao.consultarComCriterio("Estado", "nome", "Rio Grande do Sul");
 
         for (int i = 0; i < es.size(); i++) {
 
