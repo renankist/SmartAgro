1c65c75969c3bb288ca83aa2f4113ea1199652fd
RENAN LUIS KIST <rlkist@LAB59-36-WIN.ROCHAS>
2018-08-20 22:02:41 -0300

    Ajustando ícones da tela de login

--- SmartAgro/src/telas/jfrLogin.java
+++ SmartAgro/src/telas/jfrLogin.java
@@ -76,9 +76,9 @@ public class jfrLogin extends javax.swing.JFrame {
 
         jlbMsgAutenticacao.setForeground(java.awt.Color.red);
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/usuario.png"))); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/profle.png"))); // NOI18N
 
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/senha.png"))); // NOI18N
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/key.png"))); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
