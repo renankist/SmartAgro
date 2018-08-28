1bab7488998ab278bf1755f20c31c9d5455eb52f
mbagatini <morganabagatini@gmail.com>
2018-08-28 12:25:16 -0300

    Tela de Splash
    Adicionada tela para pré carregar o sistema

--- SmartAgro/src/smartagro/SmartAgro.java
+++ SmartAgro/src/smartagro/SmartAgro.java
@@ -1,14 +1,54 @@
 package smartagro;
 
+import dao.GenericDAO;
 import telas.jfrLogin;
+import javax.swing.JWindow;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 
 public class SmartAgro {
 
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new SmartAgro().start();
+            }
+        });
+    }
 
-        jfrLogin telaLogin = new jfrLogin();
+    // Tela de splash
+    JWindow window = new JWindow();
 
-        telaLogin.setVisible(true);
+    public void start() {
 
+        window.getContentPane().add(new pnlSplash());
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        
+        new SmartAgro.StartWorker().execute();
+    }
+
+    private class StartWorker extends SwingWorker<Void, Void> {
+
+        @Override
+        protected Void doInBackground() throws Exception {
+            // Faz a primeira consulta no banco para carregar o hibernate
+            new GenericDAO().consultarPorId(1, "Cidade");
+            return null;
+        }
+
+        @Override
+        protected void done() {
+
+            // Dispose window when background process is finished
+            window.dispose();
+
+            // Abre a tela de login
+            jfrLogin telaLogin = new jfrLogin();
+            telaLogin.setVisible(true);
+
+        }
     }
 }
