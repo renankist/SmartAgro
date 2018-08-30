d4efc64e31e8f0f1d6035149d89694ce8a59e4a7
Renan Luis Kist <Renan Luis Kist@179.190.67.205>
2018-08-30 20:20:38 -0300

    Inserinfo bilbiotecas.

--- SmartAgro/src/apoio/HibernateUtil.java
+++ SmartAgro/src/apoio/HibernateUtil.java
@@ -1,7 +1,7 @@
 package apoio;
 
-import org.hibernate.cfg.AnnotationConfiguration;
-import org.hibernate.SessionFactory;
+import org.hibernate.*;
+import org.hibernate.cfg.*;
 
 public class HibernateUtil {
 
@@ -9,11 +9,10 @@ public class HibernateUtil {
 
     static {
         try {
-            // Create the SessionFactory from standard (hibernate.cfg.xml) 
-            // config file.
-            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+// Create the SessionFactory from hibernate.cfg.xml
+            sessionFactory = new Configuration().configure().buildSessionFactory();
         } catch (Throwable ex) {
-            // Log the exception. 
+// Make sure you log the exception, as it might be swallowed
             System.err.println("Initial SessionFactory creation failed." + ex);
             throw new ExceptionInInitializerError(ex);
         }
